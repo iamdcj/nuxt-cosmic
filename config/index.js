8fade38a486a70fd236c392582dba70b01fd05fa
David Christian Jones <david@dcj.codes>
2020-08-20 14:47:53 -0400

    add app

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const config = {
+  bucket_name: "nuxt-cosmic",
+  bucket_slug: "nuxt-cosmic",
+  bucket_id: "5f3e9c030b7fd0000700288d",
+  read_key: "cWxFQQUxXhD4hchYdlLbufF4SVMQByAgbbhHxPqYGUUrhGJ2Wj",
+  write_key: "ac9OBIWk1ce9qWIwRWvlfLodxHxa099Zfe03TUaUrOOiQyBQVi",
+  url: "https://api.cosmicjs.com/v1/"
+}
+  
+export default config;
